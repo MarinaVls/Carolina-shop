44d94d337a98e145094b0e61bb96571b66257717
Marina <marisha.volos@gmail.com>
2022-05-26 23:32:23 +0300

    Added marginBottom fot input

--- src/Components/Forma/FormaOrder.jsx
+++ src/Components/Forma/FormaOrder.jsx
@@ -26,23 +26,22 @@ export const FormaOrder = ({onCloseAndClearBasket, orderPopUp, setOrderPopUp}) =
 
     return (
     <Form name="complex-form" onFinish={onFinish} labelCol={{span: 8}} wrapperCol={{span: 16}} initialValues={{prefix: '375(29)'}}
-            style={{width:320}}
-            >
-        <Form.Item label="Ваше имя" style={{marginBottom: 5}}>
+            style={{width:320}}>
+        <Form.Item label="Ваше имя" style={{marginBottom: 0}}>
             <Space>
                 <Form.Item name="username" rules={[{required: true, message: 'Введите Ваше имя'}]}>
-                    <Input style={{width: 230, marginBottom: 0}} placeholder="Введите Ваше имя" />
+                    <Input style={{width: 230}} placeholder="Введите Ваше имя" />
                 </Form.Item>
             </Space>
         </Form.Item>
-        <Form.Item label="Телефон" style={{marginBottom: 5}}>
+        <Form.Item label="Телефон" style={{marginBottom: 0}}>
             <Space>
                 <Form.Item name="phone" rules={[{required: true, message: 'Введите Ваш номер телефона'}]}>
                     <Input addonBefore={prefixSelector} style={{width: 230}}/>
                 </Form.Item>
             </Space>
         </Form.Item>
-        <Form.Item label="Адрес" style={{marginBottom: 5}}>
+        <Form.Item label="Адрес" style={{marginBottom: 0}}>
             <Form.Item name="country" rules={[{required: true,message: 'Введите Вашу страну'}]} 
                     style={{ display: 'inline-block', width: 230}}>
                 <Select placeholder="Введите Вашу страну">
@@ -51,13 +50,13 @@ export const FormaOrder = ({onCloseAndClearBasket, orderPopUp, setOrderPopUp}) =
                 </Select>
             </Form.Item>
             <Form.Item name='adress' rules={[{required: true, message: 'Введите Ваш адрес'}]}
-                style={{ display: 'inline-block', width: 230, marginRight:5 }}>
+                style={{ display: 'inline-block', width: 230 }}>
                 <Input placeholder="Введите Ваш адрес"/>
             </Form.Item>
         </Form.Item>
         <Form.Item label=" " colon={false} style={{marginBottom: 5}}>
             <Button type="primary" htmlType="submit" 
-                style={{height:35, width: 200, backgroundColor: 'rgb(27, 20, 100)', border: 'none', fontSize: 16}}>
+                style={{height: 35, width: 230, backgroundColor: 'rgb(27, 20, 100)', border: 'none', fontSize: 16}}>
                 Оформить заказ
             </Button>
             {orderPopUp && <OrderPopUpModal onCloseAndClearBasket={onCloseAndClearBasket} valuesModal={valuesModal} 
